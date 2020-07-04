c77ebe60eb45f32c6b9954a0905e96403ff842be
ymita <ymita0617@gmail.com>
2020-07-04 14:53:45 +0900

    ユーザー名変更

--- App/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ App/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -32,6 +32,8 @@ namespace App.Areas.Identity.Pages.Account.Manage
 
         public class InputModel
         {
+            [Display(Name = "User name")]
+            public string UserName { get; set; }
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
@@ -42,10 +44,11 @@ namespace App.Areas.Identity.Pages.Account.Manage
             var userName = await _userManager.GetUserNameAsync(user);
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
 
-            Username = userName;
+            //Username = userName;
 
             Input = new InputModel
             {
+                UserName = userName,
                 PhoneNumber = phoneNumber
             };
         }
@@ -74,6 +77,17 @@ namespace App.Areas.Identity.Pages.Account.Manage
             {
                 await LoadAsync(user);
                 return Page();
+            }
+
+            var userName = await _userManager.GetUserNameAsync(user);
+            if (Input.UserName != userName)
+            {
+                var setUserNameResult = await _userManager.SetUserNameAsync(user, Input.UserName);
+                if (!setUserNameResult.Succeeded)
+                {
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    throw new InvalidOperationException($"Unexpected error occurred setting user name for user with ID '{userId}'.");
+                }
             }
 
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
