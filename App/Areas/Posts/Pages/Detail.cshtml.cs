72b8e91e269cb1533794185c45d6dac4bb92d13e
ymita <ymita0617@gmail.com>
2020-07-09 15:10:28 +0900

    Show post detail

--- App/Areas/Posts/Pages/Detail.cshtml.cs
+++ App/Areas/Posts/Pages/Detail.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using App.Models;
+using App.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,17 @@ namespace App.Areas.Posts.Pages
 {
     public class DetailModel : PageModel
     {
-        public void OnGet()
+        private readonly IAppRepository _appRepository;
+        public Post Post { get; set; }
+        public DetailModel(IAppRepository appRepository)
         {
+            this._appRepository = appRepository;
+        }
+
+        public void OnGet(int id)
+        {
+            var post = this._appRepository.getPost(id);
+            this.Post = post;
         }
     }
 }
