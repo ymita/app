f44d19ce17faea280f452a1e1abee08c51ba6260
ymita <ymita0617@gmail.com>
2020-07-11 23:43:39 +0900

    Add a post automatically by starting a new post.

--- App/Pages/Dashboard/Posts/Edit.cshtml.cs
+++ App/Pages/Dashboard/Posts/Edit.cshtml.cs
@@ -8,16 +8,20 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using App.Data;
 using App.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace App.Pages.Dashboard.Posts
 {
     public class EditModel : PageModel
     {
         private readonly App.Data.AppDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public EditModel(App.Data.AppDbContext context)
+        public EditModel(App.Data.AppDbContext context,
+            UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         [BindProperty]
@@ -39,16 +43,51 @@ namespace App.Pages.Dashboard.Posts
             return Page();
         }
 
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostNewAsync(int id)
         {
+            if (id != -1)
+            {
+                return RedirectToPage("./Index");
+            }
+
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
-            if (Post == null)
+            // Post オブジェクトを保存
+
+            //これから編集する Post オブジェクトを用意
+            Post = new Post();
+            var userId = _userManager.GetUserId(User);
+            Post.OwnerId = userId;
+            Post.PublishedDate = DateTime.Now;
+
+            try
+            {
+                await _context.Posts.AddAsync(Post);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PostExists(Post.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            // ページ遷移
+            return Page();
+        }
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
             {
                 return Page();
             }
