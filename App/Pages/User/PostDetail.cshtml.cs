a23865bbe42d78b50f09103798708dcb0379e26e
ymita <ymita0617@gmail.com>
2020-07-24 10:53:53 +0900

    Replaced post detail page from Posts folder to User folder

--- App/Pages/User/PostDetail.cshtml.cs
+++ App/Pages/User/PostDetail.cshtml.cs
@@ -2,35 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web.Http;
 using App.Models;
 using App.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace App.Pages.Posts
+namespace App.Pages.User
 {
-    public class DetailsModel : PageModel
+    public class PostDetailModel : PageModel
     {
         private readonly IAppRepository _appRepository;
         public Post Post { get; set; }
-        public DetailsModel(IAppRepository appRepository)
+
+        public PostDetailModel(IAppRepository appRepository)
         {
             this._appRepository = appRepository;
         }
-
         public async Task<IActionResult> OnGet(string userName, int id)
         {
-            try {
+            try
+            {
                 var post = await this._appRepository.getPost(id, userName);
-                if(post.IsDraft)
+                if (post.IsDraft)
                 {
                     return NotFound();
                 }
                 this.Post = post;
             }
-            catch(HttpResponseException hrex) {
-                System.Diagnostics.Debug.WriteLine(hrex.Message);
+            catch (Exception hrex)
+            {
+                return NotFound();
             }
             return Page();
         }
