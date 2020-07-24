6f28a857b2d5164fba736258f6b27eadedc92d90
ymita <ymita0617@gmail.com>
2020-07-24 11:26:01 +0900

    Add list of posts by a specific user

--- App/Pages/User/Index.cshtml.cs
+++ App/Pages/User/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using App.Models;
+using App.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,20 @@ namespace App.Pages.User
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IAppRepository _appResository;
 
+        public IndexModel(IAppRepository appRepository)
+        {
+            this._appResository = appRepository;
+        }
+
+        public IList<Post> Posts { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var userName = User.Identity.Name;
+            var posts = await this._appResository.getPostsByUserAsync(userName);
+            Posts = posts;
         }
     }
 }
