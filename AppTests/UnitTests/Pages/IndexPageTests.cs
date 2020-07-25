37415f476a76a3c0239b3878a66e8b046cef9452
ymita <ymita0617@gmail.com>
2020-07-25 20:31:04 +0900

    Add unit tests

---
+++ AppTests/UnitTests/Pages/IndexPageTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+using App.Pages;
+using App.Repositories;
+using Moq;
+using Microsoft.Extensions.Logging;
+using App.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppTests.UnitTests.Pages
+{
+    public class IndexPageTests
+    {
+        //[Fact]
+        //public async Task OnGetAsync_PopulatesThePageModel()
+        //{
+        //    var loggerMock = new Mock<ILogger<IndexModel>>();
+
+        //    var optionsBuilder = new DbContextOptionsBuilder<AppIdentityDbContext>();
+        //    var mockAppIdentityDbContext = new Mock<AppIdentityDbContext>(optionsBuilder.Options);
+        //    var identityRepository = new Mock<IdentityRepository>(mockAppIdentityDbContext);
+        //    var appRepository = new Mock<AppRepository>();
+        //    //Arrange
+        //    var pageModel = new IndexModel(
+        //            loggerMock.Object,
+        //            identityRepository.Object,
+        //            appRepository.Object);
+        //    //Act
+        //    await pageModel.OnGet();
+
+        //    //Assert
+        //    Assert.NotNull(pageModel.Posts);
+        //    Assert.True(pageModel.Posts.Count >= 0);
+        //    Assert.NotNull(pageModel.Owners);
+        //    Assert.True(pageModel.Owners.Count >= 0);
+        //}
+    }
+}
---
+++ AppTests/UnitTests/Pages/User/IndexPageTests.cs
@@ -0,0 +1,32 @@
+﻿using App.Pages.User;
+using App.Repositories;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AppTests.UnitTests.Pages.User
+{
+    public class IndexPageTests
+    {
+        [Fact]
+        public async Task OnGetAsync_PopulatesThePageModel()
+        {
+            //    var loggerMock = new Mock<ILogger<IndexModel>>();
+
+            //var optionsBuilder = new DbContextOptionsBuilder<AppIdentityDbContext>();
+            //    var mockAppIdentityDbContext = new Mock<AppIdentityDbContext>(optionsBuilder.Options);
+            //    var identityRepository = new Mock<IdentityRepository>(mockAppIdentityDbContext);
+            var appRepository = new Mock<IAppRepository>();
+            //Arrange
+            var pageModel = new IndexModel(
+                    appRepository.Object);
+            //    //Act
+            await pageModel.OnGetAsync("abcd");
+
+            //    //Assert
+            Assert.NotNull(pageModel.Posts);
+            Assert.True(pageModel.Posts.Count >= 0);
+        }
+    }
+}
