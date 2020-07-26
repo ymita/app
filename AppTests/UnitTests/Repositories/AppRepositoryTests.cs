d956b3b0132b82a3fc9ed046c6a397b0c94868af
ymita <ymita0617@gmail.com>
2020-07-26 16:53:25 +0900

    Move unit test

---
+++ AppTests/UnitTests/Repositories/AppRepositoryTests.cs
@@ -0,0 +1,39 @@
+﻿using App.Models;
+using App.Repositories;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AppTests.UnitTests.Repositories
+{
+    public class AppRepositoryTests
+    {
+        [Fact]
+        public async Task getAllPosts()
+        {
+            //Arrange
+            var mockAppRepository = new Mock<IAppRepository>();
+
+            List<Post> posts = new List<Post>()
+            {
+                new Post{
+                    Id = 1, Title = "My first post", Description = "Body", OwnerId = "zzz", PublishedDate = DateTime.Now, UpdatedDate = DateTime.Now, IsDraft = false
+                },
+                new Post{
+                    Id = 2, Title = "My second post", Description = "Body", OwnerId = "zzz", PublishedDate = DateTime.Now, UpdatedDate = DateTime.Now, IsDraft = false
+                },
+            };
+
+            mockAppRepository.Setup(x => x.getAllPosts(false)).Returns(Task.FromResult(posts));
+
+            //Act
+            var fetchedPosts = await mockAppRepository.Object.getAllPosts(false);
+
+            //Assert
+            Assert.NotNull(fetchedPosts);
+            Assert.True(fetchedPosts.Count >= 0);
+        }
+    }
+}
